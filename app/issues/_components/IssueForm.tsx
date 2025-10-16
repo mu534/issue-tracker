09258fae410c1a79e673314bd242bb7b4e769962
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-10-16 18:30:48 +0000

    remove duplicate skeleton

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -31,7 +31,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setSubmitting(true);
       if (issue) await axios.patch(`/api/issues/${issue.id}`, data);
       else await axios.post("/api/issues", data);
-      user.push("/issues");
+      user.push("/issues/list");
     } catch (error) {
       setSubmitting(false);
       setError("An unexpected error occurred");
