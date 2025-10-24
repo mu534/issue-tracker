099eebbfb1a7f8ab32e989451b0d895cd7b09703
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-10-24 20:00:56 +0000

    Add primary key to verification_tokens table

--- prisma/migrations/20251024191938_initial_migration/migration.sql
+++ prisma/migrations/20251024191938_initial_migration/migration.sql
@@ -55,11 +55,9 @@ CREATE TABLE `users` (
 
 -- CreateTable
 CREATE TABLE `verification_tokens` (
-    `id` INT NOT NULL AUTO_INCREMENT,
     `identifier` VARCHAR(191) NOT NULL,
     `token` VARCHAR(191) NOT NULL,
     `expires` DATETIME(3) NOT NULL,
-
     UNIQUE INDEX `verification_tokens_identifier_token_key`(`identifier`, `token`),
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
