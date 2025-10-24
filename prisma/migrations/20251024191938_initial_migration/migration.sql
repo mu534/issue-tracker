6ceba1410ea8a80fec39df352eacb6d6e5bb9ce6
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-10-24 19:43:29 +0000

    fix error with prisma

--- prisma/migrations/20251024191938_initial_migration/migration.sql
+++ prisma/migrations/20251024191938_initial_migration/migration.sql
@@ -59,5 +59,6 @@ CREATE TABLE `verification_tokens` (
     `token` VARCHAR(191) NOT NULL,
     `expires` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `verification_tokens_identifier_token_key`(`identifier`, `token`)
+    UNIQUE INDEX `verification_tokens_identifier_token_key`(`identifier`, `token`),
+    PRIMARY KEY (`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
